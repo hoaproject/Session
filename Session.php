b61d07b3211a510c75ead880591f1397eb7682ba
Guiled <guislain.duthieuw@gmail.com>
2014-01-04 00:26:46 +0100

    Add __get, __set, __unset, __isset magic methods to Session as aliases to ArrayAccess methods

--- Session.php
+++ Session.php
@@ -486,6 +486,19 @@ class          Session
     }
 
     /**
+     * Check if a data exists. Alias of offsetExists.
+     *
+     * @access  public
+     * @param   mixed  $name    Data name.
+     * @return  bool
+     * @throw   \Hoa\Session\Exception\Locked
+     */
+    public function __isset( $name ) {
+
+        return $this->offsetExists($name);
+    }
+
+    /**
      * Get a data.
      *
      * @access  public
@@ -504,6 +517,19 @@ class          Session
             return null;
 
         return $this->_bucket[$offset];
+    }
+
+    /**
+     * Get a data. Alias of offsetGet.
+     *
+     * @access  public
+     * @param   mixed  $name    Data name.
+     * @return  mixed
+     * @throw   \Hoa\Session\Exception\Locked
+     */
+    public function __get( $name ) {
+
+        return $this->offsetGet($name);
     }
 
     /**
@@ -531,6 +557,22 @@ class          Session
     }
 
     /**
+     * Set a data. Alias of offsetSet.
+     *
+     * @access  public
+     * @param   mixed  $name    Data name.
+     * @param   mixed  $value     Data value.
+     * @return  \Hoa\Session
+     * @throw   \Hoa\Session\Exception\Locked
+     */
+    public function __set($name, $value) {
+
+        $this->offsetSet($name, $value);
+    }
+
+
+
+    /**
      * Unset a data.
      *
      * @access  public
@@ -549,6 +591,21 @@ class          Session
 
         return;
     }
+
+    /**
+     * Unset a data. Alias of offsetUnset.
+     *
+     * @access  public
+     * @param   mixed  $offset    Data name.
+     * @return  void
+     * @throw   \Hoa\Session\Exception\Locked
+     */
+    public function __unset( $name ) {
+
+        $this->offsetUnset($name);
+    }
+
+
 
     /**
      * Count number of data.
